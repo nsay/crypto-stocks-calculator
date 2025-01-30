f4cfe42740263dbbf5448fe4bc162b4a6970c8a9
nsay <narithsay@gmail.com>
2025-01-29 21:27:40 -0500

    fixed calculations

--- src/App.tsx
+++ src/App.tsx
@@ -9,43 +9,63 @@ const App: React.FC = () => {
   const [investment, setInvestment] = useState<number | string>("");
   const [leverage, setLeverage] = useState<number | string>("");
   const [tradingSize, setTradingSize] = useState<number | string>("");
+
   const [makerFee, setMakerFee] = useState<number | string>("");
   const [takerFee, setTakerFee] = useState<number | string>("");
   const [fundingFee, setFundingFee] = useState<number | string>("");
+  const [totalFees, setTotalFees] = useState<number>(0);
+
   const [buyPrice, setBuyPrice] = useState<number | string>("");
   const [sellPrice, setSellPrice] = useState<number | string>("");
-  const [totalFees, setTotalFees] = useState<number | null>(null);
-  const [percent, setPercent] = useState<number | null>(null);
-  const [result, setResult] = useState<number | null>(null);
 
+  const [priceChange, setPriceChange] = useState<number>(0);
+  const [unrealizedPriceChange, setUnrealizedPriceChange] = useState<number>(0);
+  const [unrealizedPnl, setUnrealizedPnl] = useState<number>(0);
+  const [realizedPriceChange, setRealizedPriceChange] = useState<number>(0);
+  const [realizedPnl, setRealizedPnl] = useState<number>(0);
+  const [result, setResult] = useState<number>(0);
   useEffect(() => {
 
-    //set fee decimals
+    //calculate fee
     const makerFeeDecimal = makerFee !== "" ? Number(makerFee) : 0;
     const takerFeeDecimal = takerFee !== "" ? Number(takerFee) : 0;
-    const fee = makerFee !== "" ? makerFeeDecimal : takerFeeDecimal;
-    const fundingFeeDecimal = Number(fundingFee);
+    const calculatedMakerTakerFee = makerFee !== "" ? makerFeeDecimal : takerFeeDecimal;
     
     //calculate trading size
     const calculatedTradingSize = Number(investment) * Number(leverage);
     setTradingSize(calculatedTradingSize);
 
     //calculate totals fees
-    const openFee = calculatedTradingSize * fee/100;
-    const totalFundingFee = calculatedTradingSize * fundingFeeDecimal/100;
-    const closingFee = calculatedTradingSize * fee/100;
-    const totalFees = openFee + totalFundingFee + closingFee;
-    setTotalFees(totalFees);
+    const totalOpenFee = (calculatedTradingSize * calculatedMakerTakerFee)/100;
+    const totalFundingFee = (calculatedTradingSize * Number(fundingFee))/100;
+    const totalClosingFee = (calculatedTradingSize * calculatedMakerTakerFee)/100;
+    const calculatedTotalFees = totalOpenFee + totalFundingFee + totalClosingFee;
+    setTotalFees(calculatedTotalFees);
 
-    //calculate totals profit/loss
-    const percentChange = ((Number(sellPrice) - Number(buyPrice)) / Math.abs(Number(buyPrice))) * 100;
-    setPercent(percentChange);
+    //calculate the percentage difference of buy and sell price
+    const calculatedPriceChange = ((Number(sellPrice) - Number(buyPrice))/Number(buyPrice))*100
+    setPriceChange(Number(calculatedPriceChange.toFixed(2)));
 
-    //calculate totals profit/loss
-    const pl = (Number(sellPrice) / Number(buyPrice)) * 100;
-    const totalProfitLoss = pl - totalFees;
+    //calculate unrealized pnl percentage
+    const calculatedUnrealizedPriceChange = ((calculatedPriceChange * Number(leverage)))/100 * Number(investment);
+    setUnrealizedPriceChange(Number(calculatedUnrealizedPriceChange.toFixed(2)));
+    
+    //calculate unrealized pnl
+    const calculatedUnrealized = (Number(investment) * calculatedUnrealizedPriceChange)/100;
+    setUnrealizedPnl(Number(calculatedUnrealized.toFixed(2)));
 
-    setResult(totalProfitLoss);
+    //calculate realized pnl percentage
+    const calculatedRealizedPriceChange = ((calculatedPriceChange * Number(leverage)))/100 * Number(investment) - calculatedTotalFees;
+    setRealizedPriceChange(Number(calculatedRealizedPriceChange.toFixed(2)));
+
+    //calculate realized pnl
+    const calculatedRealized = (Number(investment) * calculatedUnrealizedPriceChange)/100 - calculatedTotalFees;
+    setRealizedPnl(Number(calculatedRealized.toFixed(2)));
+
+    //calculate result
+    const calculatedResult = Number(investment) + Number(calculatedRealized);
+    setResult(Number(calculatedResult.toFixed(2)));
+
   }, [investment, leverage, tradingSize, makerFee, takerFee, fundingFee, buyPrice, sellPrice]);
 
   const handleMakerFeeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -160,9 +180,12 @@ const App: React.FC = () => {
         </div>
 
         <div className="mt-4">
-          <h4>Percent Change: {percent}%</h4>
-          <h4>Total Fees: {totalFees}</h4>
-          <h4>Total Profit/Loss: {result}</h4>
+          <h4>Investment Result</h4>
+          <h4>Price Change: {priceChange}%</h4>
+          <h4>Unrealized PnL: ${unrealizedPnl} ({unrealizedPriceChange}%)</h4>
+          <h4>Fees: ${totalFees}</h4>
+          <h4>Realized PnL: ${realizedPnl} ({realizedPriceChange}%)</h4>
+          <h4>Total: ${result}</h4>
         </div>
       </div>
     </Container>
